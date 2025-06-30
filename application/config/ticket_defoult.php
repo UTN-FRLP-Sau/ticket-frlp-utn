d08951f5840e25f62470c1a9e098b522426f3365
manuel-edlp <manuelmorullo.mm@gmail.com>
2025-06-30 17:02:41 -0300

    agrego variable para manejar habilitacion de compra de viandas en ambos turnos en un mismo dia

--- application/config/ticket_defoult.php
+++ application/config/ticket_defoult.php
@@ -17,6 +17,9 @@ $config['smtp_crypto'] = 'tls';
 $config['email_settings_sender'] = 'ticketweb@frlp.utn.edu.ar';
 $config['email_settings_sender_name'] = 'TicketWeb';
 
+// Permite la compra de viandas en los dos turnos en simulateneo en un mismo dia
+$config['permitir_ambos_turnos_mismo_dia'] = FALSE;
+
 // Mercado Pago Config
 $config['MP_ACCESS_TOKEN'] = '';
 $config['MP_PUBLIC_KEY'] = '';
