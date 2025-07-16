f785ef74717240a49da6f3594e6868c701eb574f
manuel-edlp <manuelmorullo.mm@gmail.com>
2025-07-16 17:49:06 -0300

    separo elementos del header

--- application/modules/usuario/views/header.php
+++ application/modules/usuario/views/header.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     <link rel="shortcut icon" type="image/png" href="<?= base_url('assets/img/utn.png'); ?>">
-    <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-2X3DWTXS1H"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
@@ -27,15 +26,15 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="<?= base_url('usuario'); ?>">Ticket</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
-                aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <div class="container"> <a class="navbar-brand" href="<?= base_url('usuario'); ?>">Ticket</a>
+            
+            <a class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
+                aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation" href="#">
                 <span class="navbar-toggler-icon"></span>
-            </button>
+            </a>
+            
             <div class="collapse navbar-collapse" id="navbarHeader">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
+                <ul class="navbar-nav ms-auto"> <li class="nav-item">
                         <a class="nav-link" href="<?= base_url('menu'); ?>">Menu</a>
                     </li>
                     <li class="nav-item">
@@ -45,35 +44,35 @@
                         <a class="nav-link" href="<?= base_url('contacto'); ?>">Contacto</a>
                     </li>
                     <?php if ($this->session->userdata('id_usuario')) : ?>
-                     <li class="nav-item">
-                        <a class="nav-link" href="<?= base_url('comentarios'); ?>">¡Haz un comentario!</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"><?= $this->session->userdata('apellido'); ?>,
-                            <?= $this->session->userdata('nombre'); ?></a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li>
-                                <a class="dropdown-item" href="<?= base_url('usuario/ultimos-movimientos'); ?>">Ultimos
-                                    movimientos</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="<?= base_url('usuario/devolver_compra'); ?>">Devolver
-                                    compras</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="<?= base_url('usuario/cambio-password'); ?>">Cambiar
-                                    contraseña</a>
-                            </li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="<?= base_url('logout'); ?>">Logout</a>
-                            </li>
-                        </ul>
-                    </li>
+                       <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url('comentarios'); ?>">¡Haz un comentario!</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"><?= $this->session->userdata('apellido'); ?>,
+                                <?= $this->session->userdata('nombre'); ?></a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li>
+                                    <a class="dropdown-item" href="<?= base_url('usuario/ultimos-movimientos'); ?>">Ultimos
+                                        movimientos</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="<?= base_url('usuario/devolver_compra'); ?>">Devolver
+                                        compras</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="<?= base_url('usuario/cambio-password'); ?>">Cambiar
+                                        contraseña</a>
+                                </li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="<?= base_url('logout'); ?>">Logout</a>
+                                </li>
+                            </ul>
+                        </li>
                     <?php endif; ?>
                 </ul>
             </div>
